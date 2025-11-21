f8f25ab67c6391346e40b0f0235bb81f3d70edc6
Wong Ho Yee <wong0382@algonquinlive.com>
2025-11-21 00:59:38 -0500

    added strapi again

--- backend/MTM6407-products/config/plugins.js
+++ backend/MTM6407-products/config/plugins.js
@@ -1,1 +1,17 @@
-module.exports = () => ({});
+module. exports = ({ env }) => ({
+    upload: {
+        config: {
+        provider: 'cloudinary',
+        providerOptions: {
+            cloud_name: env('dugtjiitzE'), 
+            api_key: env( '414693581941734'), 
+            api_secret: env ('kD4ohj53Z6K0AZYhhWaCTESX9i4'),
+        },
+        actionOptions: {
+            upload: {}, 
+            uploadStream: {}, 
+            delete: {},
+        },
+        },    
+    },
+});
