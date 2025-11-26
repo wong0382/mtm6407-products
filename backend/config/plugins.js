db1fa3d916ee07e7b13cea4b06874792c3c4ea8a
Hildy.W <wong0382@algonquinlive.com>
2025-11-25 20:04:54 -0500

    add JWT_SERET

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,17 +1,22 @@
 module.exports = ({ env }) => ({
-    upload: {
-        config: {
-        provider: 'cloudinary',
-        providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_KEY'),
-            api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-            upload: {},
-            uploadStream: {},
-            delete: {},
-        },
-        },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
+  },
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
 });
